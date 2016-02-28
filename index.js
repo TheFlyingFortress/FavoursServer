e17445b31118a7bac9b29e2616f141f673c1fe9f
Jeff Lee <jlee116057@gmail.com>
2016-02-27 18:59:51 -0800

    server request stubs are made

--- index.js
+++ index.js
@@ -8,15 +8,30 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function (socket) {
-  socket.emit('post favour', "got the req");
-  socket.on('favour', function (data) {
-    var obj = JSON.parse(data);
-    console.log(obj.uid);
-    console.log(obj.favour);
-    console.log(obj.fromWhere);
-    console.log(obj.starting);
-    console.log(obj.ending);
-    console.log(obj.meeting);
+  socket.emit('makeConnection', "got the req");
+  socket.on('postFavour', function (favour) {
+    var obj = JSON.parse(favour);
+    var uid = obj.uid;
+    var favour = obj.favour;
+    var fromWhere = obj.fromWhere;
+    var starting = obj.starting;
+    var ending = obj.ending;
+    var meeting = obj.meeting;
+    console.log(uid);
+    console.log(favour);
+    console.log(fromWhere);
+    console.log(starting);
+    console.log(ending);
+    console.log(meeting);
+  });
+  socket.on('getFavours', function (latlon) {
+    //send back json of active favours
+  });
+  socket.on('newAcc', function (userFB) {
+    //add user to the app's user db
+  });
+  socket.on('getUser', function (user) {
+    //send back json of user's info
   });
 });
 
